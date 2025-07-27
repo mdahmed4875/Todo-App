f883f5daeeaf31aa9cd1d774254b297bc9acc48c
mdahmed4875 <aahmed4875@gmail.com>
2025-07-27 11:33:01 +0530

    updated

--- script.js
+++ script.js
@@ -1,34 +1,67 @@
-let todo=document.getElementById("todo-input");
-let addTodoButton=document.getElementById("add-todo");
-addTodoButton.addEventListener("click",(e)=>{
-    e.preventDefault();
-    if(todo.value==""){
-        alert("Please enter a todo");
+
+let btn=document.getElementById('btn');
+let todayItem=document.getElementById("today-todo");
+let futureItem=document.getElementById("future-todo");
+let compltedItem=document.getElementById('completed-todo');
+let todayList=document.getElementById('today-list');
+let futureList=document.getElementById('future-list');
+let completedList=document.getElementById('completed-list');
+ const todaydate = new Date().toISOString().split('T')[0];
+btn.addEventListener('click',(e)=>{
+     e.preventDefault();
+
+    let item = document.getElementById("item").value.trim();
+    let date = document.getElementById('date').value;
+    let priority = document.getElementById('priority').value
+    if(!item||!date||!priority){
+        document.getElementById("item").value="";
+     document.getElementById('date').value="";
+     document.getElementById('priority').value="";
+        alert('enter all the inputs');
+        return;
+    }
+    else if(date<todaydate){
+        document.getElementById("item").value="";
+     document.getElementById('date').value="";
+     document.getElementById('priority').value="";
+        alert("enter valid date");
+        return;
     }
     else{
-        let todos=document.getElementById("todos");
-        let todoItem=document.createElement("div");
-        todoItem.setAttribute("class","todo-item");
-        let todoText=document.createElement("span");
-        todoText.textContent=todo.value;
-        todo.value="";
-        todoText.style.fontSize="1.2rem";
-        todoItem.appendChild(todoText);
-        let delbtn=document.createElement("button");
-        delbtn.textContent="Delete";
-        todoItem.appendChild(delbtn);
-        todos.appendChild(todoItem);
-        let donebtn=document.createElement("button");
-        donebtn.textContent="Done";
-        todoItem.appendChild(donebtn);
-        donebtn.addEventListener("click",()=>{
-            todoText.style.textDecoration="line-through";
-            todoText.style.color="#599b61ff";
-            donebtn.style.display='none';
+        let li=document.createElement('li');
+        let p1=document.createElement('p');
+        let p2=document.createElement('p');
+        let p3=document.createElement('p');
+        let deletebtn=document.createElement("button");
+        let checkbtn=document.createElement("button");
+        p1.textContent=item;
+        p2.textContent=date;
+        p3.textContent=priority;
+        deletebtn.textContent="🗑";
+        checkbtn.textContent="✔️";
+        let btns=document.createElement("div");
+        btns.appendChild(deletebtn);
+        btns.setAttribute("id","btns");
+        btns.appendChild(checkbtn);
+        li.appendChild(p1);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        li.appendChild(btns);
+        if(date==todaydate){
+            todayList.appendChild(li);
+        }
+        else{
+            futureList.appendChild(li);
+        }
+        checkbtn.addEventListener('click',(e)=>{
+            checkbtn.remove();
+            completedList.appendChild(li);
         })
-        delbtn.addEventListener("click",()=>{
-            todos.removeChild(todoItem);
+        deletebtn.addEventListener('click',(e)=>{
+            li.remove();
         })
     }
-
+     document.getElementById("item").value="";
+     document.getElementById('date').value="";
+     document.getElementById('priority').value="";
 })
